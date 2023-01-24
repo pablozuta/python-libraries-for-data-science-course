2fd5fb98ede8e93bb122304015b0cff7d655f9c9
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-01-24 13:22:26 -0400

    se actualiza seaborn file

--- sea.py
+++ sea.py
@@ -8,3 +8,14 @@ x = np.linspace(0, 10, 500)
 y = np.random.randn(500)
 plt.plot(x, y)
 plt.show()
+
+# distplot lets you not only view the histogram of a sample
+# but also estimate the distribution from which the sample is derived.
+sns.displot(y, kde=True);
+plt.show()
+
+# loading a dataset
+iris = sns.load_dataset("iris")
+iris.head()
+sns.pairplot(iris, hue='species', height=2.5);
+plt.show()
